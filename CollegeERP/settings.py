7757ca9dad60227353360f70f5e49501deb77ad9
Edidiong Etuk <edidiongmichael08@gmail.com>
2021-07-26 12:05:36 +0100

    removed debug log
    Signed-off-by: Edidiong Etuk <edidiongmichael08@gmail.com>

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jy8c-n9y=pf##!2^jae-l_5iafq6q%wfq8gdb6c0r5d52su+9y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
